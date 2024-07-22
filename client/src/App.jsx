640640f6a0f4ecf645265f41dbe68a7779b675d7
Krum Kadiyski <krum.kadiyski@gmail.com>
2024-07-22 12:50:51 +0300

    add requester

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/header/header';
+import Home from './components/home/home';
+import Login from './components/login/login';
+import Register from './components/register/register';
+import GameList from './components/game-list/game-list';
+import GameCreate from './components/game-create/game-create';
 
 function App() {
   return (
     <div id="box">
       <Header />
+
       <main id="main-content">
-        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/games" element={<GameList />} />
+          <Route path="/games/create" element={<GameCreate />} />
+        </Routes>
       </main>
     </div>
   );
--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,23 +1,29 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
-  <>
-<header>
-    <h1><a className="home" href="#">GamesPlay</a></h1>
-    <nav>
-        <a href="#">All games</a>
+    <>
+      <header>
+        <h1>
+          <Link className="home" to="/">
+            GamesPlay
+          </Link>
+        </h1>
+        <nav>
+          <Link to="/games">All games</Link>
 
-        <div id="user">
-            <a href="#">Create Game</a>
-            <a href="#">Logout</a>
-        </div>
+          <div id="user">
+            <Link to="/games/create">Create Game</Link>
+            <Link to="/logout">Logout</Link>
+          </div>
 
-        <div id="guest">
-            <a href="#">Login</a>
-            <a href="#">Register</a>
-        </div>
-    </nav>
-</header>
-  </>
-  )
-}
-export default Header
+          <div id="guest">
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+export default Header;
