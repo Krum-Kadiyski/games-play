aa06d59b0c3c661ede5606e4dff12134572a0fa3
Krum Kadiyski <krum.kadiyski@gmail.com>
2024-07-29 15:46:50 +0300

    add dynamic navigation

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/auth-context';
 
 const Header = () => {
+  const { isAutenticated } = useContext(AuthContext);
   return (
     <>
       <header>
@@ -11,16 +14,17 @@ const Header = () => {
         </h1>
         <nav>
           <Link to="/games">All games</Link>
-
-          <div id="user">
-            <Link to="/games/create">Create Game</Link>
-            <Link to="/logout">Logout</Link>
-          </div>
-
-          <div id="guest">
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-          </div>
+          {isAutenticated ? (
+            <div id="user">
+              <Link to="/games/create">Create Game</Link>
+              <Link to="/logout">Logout</Link>
+            </div>
+          ) : (
+            <div id="guest">
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </div>
+          )}
         </nav>
       </header>
     </>
