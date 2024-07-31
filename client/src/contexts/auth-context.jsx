2d358db446d38bd3c7929279442a534bb98be1ca
Krum Kadiyski <krum.kadiyski@gmail.com>
2024-07-31 12:31:46 +0300

    add : util token

--- client/src/contexts/auth-context.jsx
+++ client/src/contexts/auth-context.jsx
@@ -15,7 +15,6 @@ export const AuthContextProvider = (props) => {
   const [authState, setAuthState] = usePersistedState('auth', {});
 
   const changeAuthState = (state) => {
-    localStorage.setItem('accessToken', state.accessToken);
     setAuthState(state);
   };
 
